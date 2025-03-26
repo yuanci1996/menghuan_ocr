54ed7ed063c1460fd104a0535ad85494c37f1bce
clive <clive@v-mark.com>
2025-03-26 14:38:27 +0800

    fix: 修复只能截取主屏幕

--- src/views/common/capture_region.py
+++ src/views/common/capture_region.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import mss
 from PIL import ImageGrab
 
 
@@ -12,48 +13,83 @@ class CaptureRegion:
         self._start_x = 0
         self._start_y = 0
         self._rect = None
+        self.screens = []
+        self.active_monitor = None
+        self._capture_windows = {}  # 记录各显示器的Toplevel窗口
 
     def show_set_capture_region(self):
+        """显示截图区域选择窗口"""
         self.master.iconify()
-        self._capture_window = tk.Toplevel()
-        self._capture_window.attributes('-fullscreen', True)
-        self._capture_window.attributes('-alpha', 0.3)
-        self._capture_window.attributes('-topmost', True)
-        self._capture_window.configure(bg='black')
-        self._canvas = tk.Canvas(self._capture_window, cursor="cross", bg="black", highlightthickness=0)
+        with mss.mss() as sct:
+            self.screens = sct.monitors[1:]  # 第一个是全部屏幕, 所以跳过
 
-        self._canvas.bind("<ButtonPress-1>", self._on_button_press)
-        self._canvas.bind("<B1-Motion>", self._on_mouse_drag)
-        self._canvas.bind("<ButtonRelease-1>", self._on_button_release)
-        self._canvas.pack(fill=tk.BOTH, expand=True)
+        for monitor in self.screens:
+            top = monitor["top"]
+            left = monitor["left"]
+            width = monitor["width"]
+            height = monitor["height"]
+
+            window = tk.Toplevel()
+            window.geometry(f"{width}x{height}+{left}+{top}")
+            window.attributes('-alpha', 0.3)
+            window.attributes('-topmost', True)
+            window.configure(bg='black')
+            window.overrideredirect(True)
+            canvas = tk.Canvas(window, cursor="cross", bg="black", highlightthickness=0)
+            canvas.bind("<Enter>", lambda e, win=window: self._set_active_monitor(win))  # 鼠标进入该屏幕时激活
+            canvas.bind("<ButtonPress-1>", self._on_button_press)
+            canvas.bind("<B1-Motion>", self._on_mouse_drag)
+            canvas.bind("<ButtonRelease-1>", self._on_button_release)
+            canvas.pack(fill=tk.BOTH, expand=True)
+
+            self._capture_windows[window] = canvas  # 记录窗口
+
+    def _set_active_monitor(self, active_window):
+        """激活鼠标所在的显示器窗口"""
+        self.active_monitor = active_window
 
     def _on_button_press(self, event):
+        """记录用户的截图起始点"""
+        if not self.active_monitor:
+            return
+        self._canvas = self._capture_windows[self.active_monitor]
         self._start_x = self._canvas.canvasx(event.x)
         self._start_y = self._canvas.canvasy(event.y)
         self._rect = self._canvas.create_rectangle(self._start_x, self._start_y, self._start_x, self._start_y,
-                                                   outline="red",
-                                                   width=3)
+                                                   outline="red", width=3)
 
     def _on_mouse_drag(self, event):
+        """绘制截图选框"""
+        if not self._canvas or not self._rect:
+            return
         cur_x = self._canvas.canvasx(event.x)
         cur_y = self._canvas.canvasy(event.y)
         self._canvas.coords(self._rect, self._start_x, self._start_y, cur_x, cur_y)
 
     def _on_button_release(self, event):
+        """用户释放鼠标，确定截图区域"""
+        if not self._canvas or not self.active_monitor:
+            return
         end_x = self._canvas.canvasx(event.x)
         end_y = self._canvas.canvasy(event.y)
-        self.region = (int(min(self._start_x, end_x)),
-                       int(min(self._start_y, end_y)),
-                       int(max(self._start_x, end_x)),
-                       int(max(self._start_y, end_y)))
-        self._capture_window.withdraw()
-        self._capture_window.update()
-        if self._canvas is not None:
-            self._canvas.destroy()
+
+        monitor_index = list(self._capture_windows.keys()).index(self.active_monitor)
+        monitor = self.screens[monitor_index]
+
+        # 计算相对整个屏幕的坐标
+        self.region = (
+            int(min(self._start_x, end_x) + monitor["left"]),
+            int(min(self._start_y, end_y) + monitor["top"]),
+            int(max(self._start_x, end_x) + monitor["left"]),
+            int(max(self._start_y, end_y) + monitor["top"])
+        )
+
+        # 清理窗口
+        for win in self._capture_windows.keys():
+            win.destroy()
+        self._capture_windows.clear()
         self.master.deiconify()
-        if self._capture_window is not None:
-            self._capture_window.destroy()
         self.callback(self.region)
 
     def capture(self):
-        return ImageGrab.grab(bbox=self.region)
+        return ImageGrab.grab(bbox=self.region, all_screens=True)
